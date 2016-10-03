94967cb5e606c7ef26246b575b1e8d61b2d8524c
Manie <manie.brummer@gmail.com>
2016-10-03 13:52:07 +0200

    add the Angular Component Router and learn to navigate among the views

---
+++ app/heroes.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router"
+import { Hero } from "./hero";
+import { HeroService } from "./hero.service";
+
+@Component({
+    moduleId: module.id,
+    selector: 'my-heroes',
+    templateUrl: "heroes.component.html",
+    styleUrls: ["heroes.component.css"]
+})
+export class HeroesComponent implements OnInit {
+
+    heroes: Hero[];
+    selectedHero: Hero;
+
+    onSelect(hero: Hero): void {
+        this.selectedHero = hero;
+    };
+
+    getHeroes(): void {
+        this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+        //this.heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
+    }
+
+    constructor(private heroService: HeroService, private router: Router) {}
+
+    ngOnInit(): void {
+        this.getHeroes();
+    }
+
+    gotoDetail(): void {
+        this.router.navigate(['/detail', this.selectedHero.id]);
+    }
+}
